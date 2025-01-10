cda0355d40a60e71de981354272ad4b38389932c
akshay <akshay@gmail.com>
2025-01-10 11:11:05 +0530

    created models

--- backend/api_site/consumer/models.py
+++ backend/api_site/consumer/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
 # Create your models here.
+
+choices = (
+    ('1','type1'),
+    ('2','type2')
+)
+
+class User(models.Model):
+    c_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=15)
+    email = models.EmailField()
+    meter_type = models.CharField(max_length=10,choices=choices)
+    
+    def __str__(self):
+        return self.name
+    
+
+class MonthlyMeterReadings(models.Model):
+    meter = models.ForeignKey(User,on_delete=models.CASCADE)
+    month = models.IntegerField()
+    usage = models.FloatField() 
+    class Meta:
+        unique_together = ('meter', 'month') 
+
+
+class WeeklyMeterReadings(models.Model):
+    meter = models.ForeignKey(User,on_delete=models.CASCADE)
+    week = models.IntegerField()
+    usage = models.FloatField() 
+    class Meta:
+        unique_together = ('meter', 'week') 
+
+class DailyMeterReadings(models.Model):
+    meter = models.ForeignKey(User,on_delete=models.CASCADE)
+    hour = models.IntegerField()
+    usage = models.FloatField() 
+    class Meta:
+        unique_together = ('meter', 'hour') 
+
+
